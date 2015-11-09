8c4f3b0f7025c2dc18a8ca079324813eb4ee2967
shenhuanjie <shenhuanjie@live.cn>
2015-11-09 14:42:00 +0800

    Join to upload pictures module

---
+++ src/com/example/town_land_collect/util/PictureUtil.java
@@ -0,0 +1,142 @@
+package com.example.town_land_collect.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Base64;
+
+/**
+ * @Title PictureUtil.java
+ * @Package com.example.town_land_collect.util
+ * @Description Compressed images below 100 k and keep do not break really high
+ *              efficient method
+ * @author Shen.HuanJie
+ * @date 2015-11-9 上午11:46:32
+ * @version V1.0
+ * @ref https://github.com/feicien/StudyDemo/tree/master/FileUploadDemo/src/com/
+ *      loveplusplus/demo/fileupload/PictureUtil.java
+ */
+public class PictureUtil {
+
+	/**
+	 * 把bitmap转换成String
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	public static String bitmapToString(String filePath) {
+
+		Bitmap bm = getSmallBitmap(filePath);
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		bm.compress(Bitmap.CompressFormat.JPEG, 40, baos);
+		byte[] b = baos.toByteArray();
+
+		return Base64.encodeToString(b, Base64.DEFAULT);
+
+	}
+
+	/**
+	 * 计算图片的缩放值
+	 * 
+	 * @param options
+	 * @param reqWidth
+	 * @param reqHeight
+	 * @return
+	 */
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		// 原始图像的高度和宽度
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			// 高度和宽度的计算比率要求高度和宽度
+			final int heightRatio = Math.round((float) height / (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+			// 选择最小的比率作为inSampleSize值,这将保证一个最终的图像尺寸大于或等于请求的高度和宽度。
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+
+		return inSampleSize;
+	}
+
+	/**
+	 * 根据路径获得图片并压缩，返回bitmap用于显示
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	public static Bitmap getSmallBitmap(String filePath) {
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(filePath, options);
+
+		// 计算inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, 480, 800);
+
+		// 解码与inSampleSize设置位图
+		options.inJustDecodeBounds = false;
+
+		return BitmapFactory.decodeFile(filePath, options);
+	}
+
+	/**
+	 * 根据路径删除图片
+	 * 
+	 * @param path
+	 */
+	public static void deleteTempFile(String path) {
+		File file = new File(path);
+		if (file.exists()) {
+			file.delete();
+		}
+	}
+
+	/**
+	 * 
+	 * 说明：添加到图库
+	 * 
+	 * @param context
+	 * @param path
+	 * @return void
+	 */
+	public static void galleryAddPic(Context context, String path) {
+		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		File f = new File(path);
+		Uri contentUri = Uri.fromFile(f);
+		mediaScanIntent.setData(contentUri);
+		context.sendBroadcast(mediaScanIntent);
+	}
+
+	/**
+	 * 获取保存图片的目录
+	 * 
+	 * @return
+	 */
+	public static File getAlbumDir() {
+		File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), getAlbumName());
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		return dir;
+	}
+
+	/**
+	 * 获取保存 隐患检查的图片文件夹名称
+	 * 
+	 * @return
+	 */
+	public static String getAlbumName() {
+		return "upload";
+	}
+}
+
